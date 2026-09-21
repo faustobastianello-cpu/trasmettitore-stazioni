ed14210f39a3a7bafa82db57a45fa210233c5654
faustobastianello-cpu <fausto.bastianello@gmail.com>
2026-09-21 22:23:56 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+let forza = 0
+// Entrambi i micro:bit devono avere lo stesso gruppo radio
+radio.setGroup(1)
+let trasmesso = 0
+basic.forever(function () {
+    forza = input.magneticForce(Dimension.Strength)
+    if (forza > 300) {
+        basic.showIcon(IconNames.Heart)
+        if (trasmesso == 0) {
+            radio.sendString("4")
+            trasmesso = 1
+        }
+    } else {
+        basic.clearScreen()
+        trasmesso = 0
+    }
+})
